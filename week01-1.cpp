283b0c5b7725c6174ea527edd8e1f677b7e26b5c
awen0407 <ben930407@gmail.com>
2025-02-21 20:48:48 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,10 @@
+/// week01-1.cpp 要自己打好全部的檔名,後面要打.cpp
+/// 在瘋狂程設(練習模式)寫好的程式碼,全選Ctrl-C
+/// CodeBlocks File-New-Empty 空白檔案,Ctrl-S存檔
+#include <stdio.h> /// 使用 standard 標準的 input/output
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("%d",a+b);
+}
